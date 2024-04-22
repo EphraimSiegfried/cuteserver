6da67d99e68951f2f15ffe7e1dc67752afeec43b
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-04-22 20:43:10 +0200

    add logging library

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build_server: 
-	gcc -o server.o src/main.c src/parser.c src/response.c 
+	gcc -o server.o src/main.c src/parser.c src/response.c src/log.c -DLOG_USE_COLOR
 
 clean: 
 	rm -f server.o 
--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
+#include "log.h"
 #include "response.h"
 #include <arpa/inet.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/errno.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
@@ -15,8 +17,6 @@ void *serve(void *client_sock) {
     char buff[MAX_BUF_SIZE];
     int ret = read(*client_socket, buff, sizeof(buff));
     int request_line_indices[3];
-    printf("\n\nTHE REQUEST:\n %s \n\n",buff);
-    fflush(stdout);
     int index = 0;
     //TODO: error handling for reading from buffer
     for (size_t i = 0; i < ret; i++) {
@@ -30,24 +30,25 @@ void *serve(void *client_sock) {
             break;
         }
     }
+    log_info("%*.*s", request_line_indices[2], request_line_indices[2], buff);
     if (strncmp(buff, "GET", request_line_indices[0]) == 0) {
         char file_path[MAX_BUF_SIZE];
-        int file_path_len = request_line_indices[1] - request_line_indices[0]; 
-        strncpy(file_path, &buff[request_line_indices[0]+1], file_path_len-1); // +1 because of whitespace
+        int file_path_len = request_line_indices[1] - request_line_indices[0];
+        strncpy(file_path, &buff[request_line_indices[0] + 1], file_path_len - 1);// +1 because of whitespace
         printf("%s", file_path);
         fflush(stdout);
-        if (strlen(file_path) == 1 && strncmp(file_path,"/", 1) == 0){ 
-            sprintf(file_path, "%s", "/index.html"); 
-        } 
-        printf("\nFILE PATH: %s, LENGTH: %d", file_path, strlen(file_path));
-        fflush(stdout); 
-        if (access(file_path, F_OK) != 0){
-            printf("\naccess not granted\n"); 
-            send_error(*client_socket,404);
+        if (strlen(file_path) == 1 && strncmp(file_path, "/", 1) == 0) {
+            sprintf(file_path, "%s", "/index.html");
+        }
+        printf("\nFILE PATH: %s, LENGTH: %lu", file_path, strlen(file_path));
+        fflush(stdout);
+        if (access(file_path, F_OK) != 0) {
+            printf("\naccess not granted\n");
+            send_error(*client_socket, 404);
             return NULL;
         }
-        send_ok(*client_socket, file_path); //TODO: handle other mime types 
-    } 
+        send_ok(*client_socket, file_path);//TODO: handle other mime types
+    }
     // TODO: hashmap info = parse_request(*buff)
     return NULL;
 }
@@ -55,14 +56,14 @@ void *serve(void *client_sock) {
 int main(void) {
 
     if (chroot("./data") < 0) {
-        perror("Error");
+        log_error("Chroot error: %s", strerror(errno));
     }
     // SERVER SOCKET
     struct sockaddr_in server_addr;
 
     // PF_INET= ipv4, SOCK_STREAM=tcp
     int server_sock = socket(PF_INET, SOCK_STREAM, 0);
-    printf("Server has started\n");
+    log_info("Server has started");
 
     server_addr.sin_family = AF_INET;
     // htons= host to network short
@@ -70,13 +71,11 @@ int main(void) {
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     if (bind(server_sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-        printf("Couldn't connect to the port");
-        fflush(stdout);
+        log_fatal("Binding error: %d", strerror(errno));
         exit(EXIT_FAILURE);
     }
     if (listen(server_sock, MAX_PENDING) < 0) {
-        printf("Error while listening");
-        fflush(stdout);
+        log_fatal("Listening error %d", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
@@ -87,11 +86,10 @@ int main(void) {
         client_sock =
                 accept(server_sock, (struct sockaddr *) &client_addr, &client_addr_len);
 
-        printf("\nconnected to %d\n", client_addr.sin_addr.s_addr);
-        fflush(stdout);
+        log_info("New connection accepted from %s:%d", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
         if (client_sock < 0) {
-            printf("failed to accept");
+            log_fatal("Accepting Error: %d", strerror(errno));
             exit(EXIT_FAILURE);
         }
 
