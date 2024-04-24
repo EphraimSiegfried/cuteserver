f7b6d59b83fc6f06ec36147df498a7648aacef0d
rahelv <89937339+rahelv@users.noreply.github.com>
2024-04-24 22:51:01 +0200

    add parsing of headers

--- src/parser.c
+++ src/parser.c
@@ -1,12 +1,30 @@
 #include "parser.h"
+#include "../deps/hashmap/sc_map.h"
 #include "../deps/log/log.h"
+#include "utils.h"
 #include <string.h>
 
+int parse_headers(char *buf, request_info *req_i) {
+    struct sc_map_str map;
+    const char *key, *value;
+    sc_map_init_str(&map, 0, 0);
+    char *temp_buf = buf;
+    char *line;
+    while ((line = strsep(&temp_buf, "\n")) != NULL) {//TODO: find better implementation
+        if (strcmp(line, "") == 0 || strcmp(line, " ") == 0 || strcmp(line, "\r") == 0) continue;
+        if (((key = strtok(line, ": ")) == NULL) || ((value = trim(strtok(NULL, ": "))) == NULL)) return -1;
+        sc_map_put_str(&map, key, value);
+    }
+    req_i->headers = map;
+    // sc_map_foreach(&map, key, value) {
+    //     log_debug("KEY: [%s] | VALUE: [%s]", key, value);
+    // }
+    return 1;
+}
 
-int parse_request(char *buff, int buf_length, request_info *req_i) {
+int parse_request_line(char *buff, int buf_length, request_info *req_i) {
     int request_line_indices[3];
     int index = 0;
-    log_debug("parse request");
     //TODO: error handling for reading from buffer
     for (int i = 0; i < buf_length; i++) {
         if (buff[i] == ' ') {//TODO: refactor
@@ -33,5 +51,5 @@ int parse_request(char *buff, int buf_length, request_info *req_i) {
     } else {
         return -1;
     }
-    return 1;
+    return request_line_indices[2] + 2;
 }
--- src/parser.h
+++ src/parser.h
@@ -2,20 +2,22 @@
 #define WEBSERVER_PARSER_H
 
 #define TODO_SIZE 6342
+#include "../deps/hashmap/sc_map.h"
 
 enum req_type {
-    GET, 
-    POST, 
+    GET,
+    POST,
     PUT
 };
 
 typedef struct {
     enum req_type type;
-    char file_path[TODO_SIZE]; 
-    char version[TODO_SIZE]; 
-    char rest[TODO_SIZE]; // TODO: unpack rest of buffer 
+    char file_path[TODO_SIZE];
+    char version[TODO_SIZE];
+    struct sc_map_str headers;
 } request_info;
 
-int parse_request(char *buff, int buf_length, request_info * req_info);
+int parse_request_line(char *buff, int buf_length, request_info *req_info);
 
-#endif //WEBSERVER_PARSER_H
+int parse_headers(char *buf, request_info *req_info);
+#endif//WEBSERVER_PARSER_H
