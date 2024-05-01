1fd49167be5973aa1ae9e8e51b6441a5a65a55ba
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-05-01 09:58:56 +0200

    add cgi support

--- data/cgi-bin/cgi.c
+++ data/cgi-bin/cgi.c
@@ -1,14 +1,21 @@
-//TODO: make into .cgi
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
 int main() {
     char *env_value;
     char *save_env;
     time_t current_time;
 
     printf("Content-type: text/html\n\n");
+    printf("<html>\n");
+    printf("<head>\n");
+    printf("<title>Improved Simple Sample</title>\n");
+    printf("</head> <body>\n");
+    printf("<h1>This is a Heading</h1>\n");
+    printf("<p> <pre>\n");
     printf("C Program Version\n");
     current_time = time(NULL); /* get current time */
     printf("It is now %s\n", ctime(&current_time));
@@ -22,6 +29,7 @@ int main() {
     printf("The query is %s\n", env_value); /* and print it */
     printf("You are signed onto %s\n", getenv("REMOTE_HOST"));
     printf("Your IP Address is %s\n", getenv("REMOTE_ADDR"));
+    printf("</pre> </body> </html>\n");
     fflush(stdout); /* force physical write */
     exit(0);
 }
