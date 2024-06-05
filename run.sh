a28f8edfd938e4f77f13483b1bb96f60dc8160e3
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-06-05 08:01:20 +0200

    add clear

--- run.sh
+++ run.sh
@@ -6,5 +6,6 @@ docker build -t cutie .
 # Build the image for the example app
 docker build -t kitty_chat example_app/
 
+clear
 # Run the example app in a container
 docker run -p 8888:80 kitty_chat
